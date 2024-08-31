c36ace672bf30be42bd8f278cc573c692406ae29
Erastus Doh <erastusdoh@gmail.com>
2024-08-31 19:18:57 +0100

    Completed youtube tutorial on supabase

---
+++ src/components/SmoothieCard.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import supabase from "../config/supabaseClient"
+
+const SmoothieCard = ({smoothie, onDelete}) => {
+    const navigate = useNavigate()
+    const handleDelete = async() => {
+        const {data, error} = await supabase
+        .from("smoothies")
+        .delete()
+        .eq("id", smoothie.id)
+        .select()
+
+        if (error){
+            console.log(error)
+            return
+        }
+
+        if (data){
+            console.log(data)
+            onDelete(smoothie.id)
+            navigate("/")
+
+        }
+    }
+
+
+    return (
+        <div className="smoothie-card">
+            <h3>{smoothie.title}</h3>
+            <p>{smoothie.method}</p>
+            <div className="rating">{smoothie.rating}</div>
+            <div className="buttons">
+                <Link to={"/" + smoothie.id}>
+                    <i className="material-icons">edit</i>
+                </Link>
+                <i className="material-icons" onClick={handleDelete}>delete_forever</i>
+            </div>
+        </div>
+    )
+}
+
+export default SmoothieCards
